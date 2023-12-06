76f036fcbbc1626c298c4e0aed17c263cccc0e64
Rohama-Majeed7 <139218403+Rohama-Majeed7@users.noreply.github.com>
2023-12-06 22:43:32 +0500

    change

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 function toggleAccordion(element) {
   // Get the next sibling, which is the content div
   var content = element.nextElementSibling;
-
+  window.addEventListener("DOMContentLoaded", () => {
+    content.style.display = "block";
+  });
   // Toggle the display of the content
   if (content.style.display === "block") {
     content.style.display = "none";
